329d0251aa2b432776a2036af1a64eb259873f01
F3719E <F3719E@GMAIL.COM>
2024-05-20 14:31:52 -0400

    se agrega smootjscroll, readme y link paginas desde iconos del footer

--- assets/js/Viajes.js
+++ assets/js/Viajes.js
@@ -16,48 +16,48 @@ $(document).ready(function () {
   });
 });
 
-$(document).ready(function () {
-  $("#tituloingredientes").on("dblclick", function () {
-    $("table#tablaingredientes td").css("color", "red");
-    $("table#tablaingredientes1 td").css("color", "red");
+// $(document).ready(function () {
+//   $("#tituloingredientes").on("dblclick", function () {
+//     $("table#tablaingredientes td").css("color", "red");
+//     $("table#tablaingredientes1 td").css("color", "red");
 
-  });
-});
+//   });
+// });
 
-$(document).ready(function () {
-  $("#titulopreparacion").on("dblclick", function () {
-    $("table#tablapreparacion td").css("color", "red");
+// $(document).ready(function () {
+//   $("#titulopreparacion").on("dblclick", function () {
+//     $("table#tablapreparacion td").css("color", "red");
 
-  });
-});
-
-
-$(document).ready(function () {
-
-  $("#titulocard").click(function () {
-    $("#cartatexto1").toggle();
-    $("#cartatexto2").toggle();
-    $("#cartatexto3").toggle();
-  });
-});
+//   });
+// });
 
 
-$("#nav ul li a[href^='#']").on('click', function(e) {
+// $(document).ready(function () {
 
-  // prevent default anchor click behavior
-  e.preventDefault();
+//   $("#titulocard").click(function () {
+//     $("#cartatexto1").toggle();
+//     $("#cartatexto2").toggle();
+//     $("#cartatexto3").toggle();
+//   });
+// });
 
-  // store hash
-  var hash = this.hash;
 
-  // animate
-  $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 1000, function(){
+// $("#nav ul li a[href^='#']").on('click', function(e) {
 
-      // when done, add hash to url
-      // (default click behaviour)
-      window.location.hash = hash;
-    });
+//   // prevent default anchor click behavior
+//   e.preventDefault();
 
-});
+//   // store hash
+//   var hash = this.hash;
+
+//   // animate
+//   $('html, body').animate({
+//       scrollTop: $(hash).offset().top
+//     }, 1000, function(){
+
+//       // when done, add hash to url
+//       // (default click behaviour)
+//       window.location.hash = hash;
+//     });
+
+// });
---
+++ doc/Readme.md
@@ -0,0 +1,15 @@
+Simple text 
+# primero
+## viajes.js
+se usan funciones para los tooltip, boton enviar correo.
+
+
+## viajes.css
+se clases  para los ajustar estilos para dar formato al UI.
+
+
+## viajes.html
+
+se crean los distintos elementos solicitados.
+
+se utilizan importacion de bootstrap, fontawesome , jquery, google font.
